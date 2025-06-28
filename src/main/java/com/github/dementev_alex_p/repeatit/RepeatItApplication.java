04a166605317a576faab7827b9916dcdbe8ef276
dementev.alex.p <dementev.alex.p@gmail.com>
2025-06-28 19:35:41 +0300

    init commit

---
+++ src/main/java/com/github/dementev_alex_p/repeatit/RepeatItApplication.java
@@ -0,0 +1,13 @@
+package com.github.dementev_alex_p.repeatit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepeatItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepeatItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/dementev_alex_p/repeatit/RepeatItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dementev_alex_p.repeatit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepeatItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
